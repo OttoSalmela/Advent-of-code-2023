fad6d0427b3f2fcb9b28459e8049db899ebd046b
Otto Salmela <otto.salmela@outlook.com>
2023-12-23 19:43:37 +0200

    3

--- Day3/day3.py
+++ Day3/day3.py
@@ -1,2 +1,23 @@
 file = open('Day3/Day3.txt', 'r')
-input = file.read()
+lines = file.readlines()
+
+array = []
+for line in lines:
+    line_array = []
+    for char in line:
+        if char != "\n":
+            line_array.append(char)
+    array.append(line_array)
+
+stars = []
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][j] == '*':
+            stars.append([i,j])
+
+numbers = []
+number = []
+for i in range(len(array)):
+    for j in range(len(array[i])):
+        if array[i][j].isnumeric():
+            number.append[array[i][j]]
